7a318cc3a6479a7587acffad3ce7980734af33ce
diegoarielsimonelli <simonellidiego@gmail.com>
2021-08-04 17:37:46 -0300

    1

---
+++ src/components/NavBar.js
@@ -0,0 +1,51 @@
+
+
+const NavBar = () => {
+    return ( 
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+  
+    <a className="navbar-brand" href>C-Design</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href>Productos</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href>Nosotros</a>
+        </li>
+        
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+    
+        
+      // <div>
+
+      // <ul className="Nav">
+      //   <li >
+      //     <a >C-Design</a>
+      //   </li>
+      //   <li>
+      //     <a>Nosotros</a>
+      //   </li>
+      //   <li>
+      //     <a>Productos</a>
+      //   </li>
+          
+      // </ul>
+      // </div>
+    
+    ) 
+    
+  
+  ;
+  };
+  
+  export default NavBar;
+  
